011c894c2932b8c88d129aa989ee1bd8eb0ee979
Jan Schumann <js@schumann-it.com>
2012-06-11 12:48:26 +0200

    migration to support long configuration values (column type text)

---
+++ db/migrate/20120611103428_change_data_type_configuration_values.rb
@@ -0,0 +1,9 @@
+class ChangeDataTypeConfiurationValues < ActiveRecord::Migration
+  def self.up
+    change_column :configuration_parameters, :value, :text
+  end
+
+  def self.down
+    change_column :configuration_parameters, :value, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120423145516) do
+ActiveRecord::Schema.define(:version => 20120611103428) do
 
   create_table "configuration_parameters", :force => true do |t|
     t.string   "name"
