851d974c69491fe755db33e7bdcd02584bdda8ea
Jan Schumann <js@schumann-it.com>
2013-02-12 10:44:37 +0100

    Update config/initializers/capistrano_namespace_rake_fix.rb

--- config/initializers/capistrano_namespace_rake_fix.rb
+++ config/initializers/capistrano_namespace_rake_fix.rb
@@ -1,5 +1,5 @@
 # see http://groups.google.com/group/capistrano/browse_thread/thread/b5e11c0ebf37a8be
-Capistrano::Configuration::Namespaces::Namespace.class_eval do
-  undef :symlink
-  undef :install
-end
+#Capistrano::Configuration::Namespaces::Namespace.class_eval do
+#  undef :symlink
+#  undef :install
+#end
